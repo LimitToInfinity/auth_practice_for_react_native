e177bd2a8ad586f686e3b1b453a74775f22189f1
LimitToInfinity <21stLimitless@gmail.com>
2020-08-17 13:41:29 -0600

    Adds has_secure_password in User model, adds index and create actions in UsersController, and modifies routes for users to only index and create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(
+      username: params[:username],
+      password: params[:password]
+    )
+  end
+
 end
