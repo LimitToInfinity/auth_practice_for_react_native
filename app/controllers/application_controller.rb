2cc71dcf8c9709adfb7aebfe7dee42dea7e6a166
LimitToInfinity <21stLimitless@gmail.com>
2020-08-17 14:37:55 -0600

    Adds authenticate middleware with check for Authorization header token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  before_action :authenticate
+
+  def authenticate
+    auth_header = request.headers['Authorization']
+
+    if !auth_header
+      render json: { message: 'No token' }, status: :forbidden
+    else
+      render json: { message: 'cool token' }, status: :ok
+    end
+  end
+
 end
